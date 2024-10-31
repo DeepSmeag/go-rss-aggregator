651c5da20e75528ed9294dce614d2c105b539ae4
Cosmin <cosmin072@gmail.com>
2024-10-31 12:56:14 +0200

    dotenv

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello world")
+	godotenv.Load() // load .env by default; otherwise specify file path
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in environment")
+	}
+	fmt.Println("Port:", portString)
 }
